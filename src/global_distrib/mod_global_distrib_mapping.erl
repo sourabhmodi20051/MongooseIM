f785f52163332c903004e846a97d632f66a1afdc
kanes115 <dominikstanaszek@icloud.com>
2018-02-03 10:25:11 +0100

    Add spec to hosts/0 function

--- src/global_distrib/mod_global_distrib_mapping.erl
+++ src/global_distrib/mod_global_distrib_mapping.erl
@@ -120,6 +120,7 @@ all_domains() ->
 endpoints(Host) ->
     mod_global_distrib_mapping_backend:get_endpoints(Host).
 
+-spec hosts() -> [Host :: jid:lserver()].
 hosts() ->
     mod_global_distrib_mapping_backend:get_hosts().
 
